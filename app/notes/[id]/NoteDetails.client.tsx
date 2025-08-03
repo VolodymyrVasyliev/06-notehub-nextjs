19fcb537ee07337bdc494af6a5fe721c59e1352e
Volodymyr Vasyl'ev <vasyliev.official@gmail.com>
2025-08-03 16:46:08 +0300

    id note

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,12 +1,9 @@
 'use client';
 import React from 'react';
 import css from './NoteDetails.module.css';
-import type { Note } from '@/type/note';
 import { fetchNoteById } from '@/lib/api';
 import { useParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
-
-import { useState } from 'react';
 
 
 
@@ -14,18 +11,19 @@ const NoteDetailsClient = () => {
     const { noteId } = useParams<{ noteId: string }>()
     
       const { data: note } = useQuery({
-        queryKey: ['note', noteId],
+        queryKey: ['notes', noteId],
         queryFn: () => fetchNoteById(noteId),
         refetchOnMount: false,
       });
+    
   return (
     <div className={css.container}>
       <div className={css.item}>
         <div className={css.header}>
-          <h2>{note.title}</h2>
+          <h2>{note?.title}</h2>
         </div>
-        <p className={css.content}>{note.content}</p>
-        <p className={css.date}>{note.createdAt}</p>
+        <p className={css.content}>{note?.content}</p>
+        <p className={css.date}>{note?.createdAt}</p>
         createdAt
       </div>
     </div>
--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,27 +1,32 @@
-import css from './NoteDetails.module.css';
+// import css from './NoteDetails.module.css';
 import { fetchNoteById } from '@/lib/api';
-import { QueryClient } from '@tanstack/react-query';
+import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import NoteDetailsClient from './NoteDetails.client';
-// HydrationBoundary;
-// dehydrate;
+
 type Props = {
   params: Promise<{ noteId: string }>;
 };
 
 const NoteDetails = async ({ params }: Props) => {
   const { noteId } = await params;
-  const note = await fetchNoteById(noteId);
   console.log(noteId);
   
 
   const queryClient = new QueryClient();
+
+
   await queryClient.prefetchQuery({
-    queryKey: ['note', noteId],
+    queryKey: ['notes', noteId],
     queryFn: () => fetchNoteById(noteId),
   });
 
 
-  return <NoteDetailsClient note={note}  />;
+  return (
+     <HydrationBoundary state={dehydrate(queryClient)}>
+    < NoteDetailsClient />
+    </HydrationBoundary>
+
+  )
 };
 
 export default NoteDetails;
